7b27e4676af2cb5368762a0a322d00a78db87573
Peter Park 2.0 <peterparkk25+@gmail.com>
2021-01-01 19:16:06 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -1,11 +1,9 @@
 def third_challenge
   epic_tragedy = {
    :montague => {
-      :patriarch => {
-        #your key/value pairs here
+      :patriarch => {name: "Lord Montague", age:"53"
         },
-      :matriarch => {
-        #your key/value pairs here
+      :matriarch => {name: "Lady Montague", age:"54"
         },
       :hero => {
         #your key/value pairs here
